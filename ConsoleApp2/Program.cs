6c8371f11cc5103a6faf8b66d2b39d914d7b1e28
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-11-30 09:42:15 +0100

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Tävling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int AntalMorötter = 40;
+
+            int Loop = 1;
+
+            int TorsAntal = 0;
+            int MorsAntal = 0;
+
+            Console.Write("Tors tid?: "); int TorsTid = int.Parse(Console.ReadLine());
+            Console.Write("Mors tid?: "); int MorsTid = int.Parse(Console.ReadLine());
+
+            while (AntalMorötter >= 1)
+            {
+                if (AntalMorötter == 1 && Loop % TorsTid == 0 && Loop % MorsTid == 0)
+                {
+                    AntalMorötter--;
+                }
+                else if (Loop % TorsTid == 0 && Loop % MorsTid == 0)
+                {
+                    TorsAntal++;
+                    AntalMorötter--;
+                    MorsAntal++;
+                    AntalMorötter--;
+                }
+                else if (Loop % TorsTid == 0)
+                {
+                    TorsAntal++;
+                    AntalMorötter--;
+                }
+                else if (Loop % MorsTid == 0)
+                {
+                    MorsAntal++;
+                    AntalMorötter--;
+                }
+
+                Loop++;
+            }
+
+            Console.WriteLine($"Svar:\n- Tor: {TorsAntal}\n- Mor: {MorsAntal}");
+        }
+    }
+}
